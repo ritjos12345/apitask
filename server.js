837ddd9d4a0c5687bb0dac0448a95223ae3ad25b
Ritesh Joshi <ritjos12345@gmail.com>
2019-08-14 15:10:07 +0530

    update IP

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose = require('mongoose'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dbrgvd:Welcome.1@52.74.229.193:27017/cabs_db', { useNewUrlParser: true });
+mongoose.connect('mongodb://dbrgvd:Welcome.1@127.0.0.1:27017/cabs_db', { useNewUrlParser: true });
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
